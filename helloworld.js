73dc73d1f346d36a7f99b62e263eafd4ed4dc12e
Lucas <lucas.geurtjens@griffithuni.edu.au>
2019-07-19 10:52:05 +1000

    Added a function for adding numbers

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,8 @@
 console.log("I caused a merge conflict");   //haha get hacked kid
+
+function add_numbers(num1, num2) {
+	return num1 + num2;
+}
+
+console.log("hello world");
+console.log(add_numbers(5, 10));
